44683d35dd34601a09757470026b76a0a371dd89
Nestor Cofre <50756891+ncofre98@users.noreply.github.com>
2025-03-28 17:16:15 -0600

    Added instructions

--- lib/UI.rb
+++ lib/UI.rb
@@ -1,18 +1,51 @@
 module UI
   def self.select
     puts "Make your choice:"
-    MasterMind::COLORS.each_with_index {|color, index| puts "#{color} = #{index}"}
-    gets_chomp.to_i
+    show_options
+    gets.chomp.to_i
+  end
+
+  def self.select_hidden_code
+    puts "Insert your hidden code:"
+    show_options
+    gets.chomp
+  end
+
+  def self.show_instructions
+    puts <<~INSTRUCTIONS
+    🎯 **Goal of the Game:**  
+    Discover the secret code before running out of your 12 attempts.  
+
+    🔥 **How to Play:**  
+    - Enter 5 numbers, one for each colored ball.  
+    - Each number corresponds to a specific color.  
+
+    🛠️ **Color Options:***
+    INSTRUCTIONS
+
+    show_options
+
+    puts <<~EXAMPLE
+    ✅ **Example:**  
+    If you enter: `01234`  
+    You'll see: 🔴🔵🟢🟡🟠  
+
+    Good luck! 🍀
+    EXAMPLE
+  end
+
+  def self.show_options
+    puts MasterMind::COLORS.map.with_index {|color, index| "#{color} = #{index}"}.join(' ')
   end
 
   def self.get_name
     puts "Insert name of player:"
-    gets_chomp
+    gets.chomp
   end
 
   def self.get_number_of_players
     puts "Select number of players (1 - 2):"
-    gets_chomp.to_i
+    gets.chomp.to_i
   end
 
   def self.show_board(board)
--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -3,14 +3,23 @@ class MasterMind
   EXACT_MATCH = '⚫'
   COLOR_MATCH = '⚪'
 
-  attr_accessor :board, :player1, :player2
+
+  attr_accessor :board, :player1, :player2, :guess
+  attr_reader :hidden_code, :number_of_players
   def initialize
     @board = Board.new
     @number_of_players = UI.get_number_of_players
     @player1 = Player.new(UI.get_name)
-    @player2 = @number_of_players == 2 ? Player.new(UI.get_name) : Player.new 
+    @player2 = @number_of_players == 1 ? Player.new : Player.new(UI.get_name)
   end
 
   def play
+    turns = 12
+    hidden_code = number_of_players == 1 ? COLORS.sample(5) : UI.select
+ 
+    while (turns > 0)
+      
+      turns -= 1
+    end
   end
 end
