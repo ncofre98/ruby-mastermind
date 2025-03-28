c6ab47aa8ae6efc5549ce0096dcad2b815f5e7b6
Nestor Cofre <50756891+ncofre98@users.noreply.github.com>
2025-03-28 16:07:37 -0600

    Fixed invalid emojis

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,8 +1,16 @@
 class MasterMind
-  COLORS = [🔴, 🔵, 🟢, 🟡, 🟠, 🟣, ⚫, ⚪]
-  EXACT_MATCH = ⚫
-  COLOR_MATCH = ⚪
+  COLORS = ['🔴', '🔵', '🟢', '🟡', '🟠', '🟣', '⚫', '⚪']
+  EXACT_MATCH = '⚫'
+  COLOR_MATCH = '⚪'
 
+  attr_accessor :board, :player1, :player2
   def initialize
+    @board = Board.new
+    @number_of_players = UI.get_number_of_players
+    @player1 = Player.new(UI.get_name)
+    @player2 = @number_of_players == 2 ? Player.new(UI.get_name) : Player.new 
+  end
+
+  def play
   end
 end
